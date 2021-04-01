8489d7a13a49a58433f9b974977f75035d1b64ba
G Andrienko <gennady.andrienko@iais.fraunhofer.de>
2021-04-01 17:13:51 +0200

    data structures (in progress)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+class flight {
+  String id;
+  int maxdelay=0;
+  int delays[]=null;
+
+  public flight (String id) {
+    this.id=id;
+    delays=new int[1440];
+    for (int i=0; i<delays.length; i++)
+      delays[i]=0;
+  }
+}
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
 }
